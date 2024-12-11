4a274858fc295d39c758609a646b84cf32c8dd42
Akio Taniguchi <13254278+astropenguin@users.noreply.github.com>
2024-12-11 08:57:15 +0000

    #93 Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,25 +1,31 @@
-# Project information
-project = "deshima-sensitivity"
-copyright = "2020 DESHIMA software team"
-author = "Akira Endo"
-release = "0.3.1"
+# project information
+author = "DESHIMA Team"
+copyright = "2020-2024 DESHIMA Team"
 
 
-# General configuration
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
+# general configuration
+add_module_names = False
+autodoc_typehints = "both"
+autodoc_typehints_format = "short"
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
 ]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
+myst_heading_anchors = 3
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
-# Options for HTML output
-html_static_path = ["_static"]
-html_logo = "_static/logo.png"
+# options for HTML output
 html_theme = "pydata_sphinx_theme"
 html_theme_options = {
     "github_url": "https://github.com/deshima-dev/deshima-sensitivity/",
+    "logo": {"text": "deshima-sensitivity"},
 }
